688189bb041a01c133eb34fee4d2fd8efe42e885
Nihal-prog <nihalsingh010884@gmail.com>
2026-09-05 10:06:25 +0530

    Compound operators in C

---
+++ compound-operators.c
@@ -0,0 +1,22 @@
+/* Compound/Short-hand assignment operators */
+
+#include <stdio.h>
+
+int main()
+{
+    int num = 9;
+    printf("%d\n", num);
+
+    num += 12;
+    printf("%d\n", num);
+    
+    num -= 12;
+    printf("%d\n", num);
+
+    num /= 1;
+    printf("%d\n", num);
+
+    num *= 1;
+    printf("%d\n", num);
+    return 0;
+}
