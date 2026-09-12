9eaa6f4b96caf41375cb1f473d5dfccd2adaf79e
Nihal-prog <nihalsingh010884@gmail.com>
2026-09-12 08:32:14 +0530

    Area of square using math.h library

---
+++ area-of-square.c
@@ -0,0 +1,10 @@
+/* Using the <math.h> library to calculate the area of a square. */
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int a = 5;
+    printf("Area of square with side 5 is: %f.\n", pow(a, 2));
+    return 0;
+}
